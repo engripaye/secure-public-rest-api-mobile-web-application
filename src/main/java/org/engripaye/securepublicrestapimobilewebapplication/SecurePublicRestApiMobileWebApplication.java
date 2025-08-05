76076798a6b24b3c668fe61d4a0aac9981824579
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-05 19:02:29 +0100

    first commit

---
+++ src/main/java/org/engripaye/securepublicrestapimobilewebapplication/SecurePublicRestApiMobileWebApplication.java
@@ -0,0 +1,13 @@
+package org.engripaye.securepublicrestapimobilewebapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurePublicRestApiMobileWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurePublicRestApiMobileWebApplication.class, args);
+    }
+
+}
