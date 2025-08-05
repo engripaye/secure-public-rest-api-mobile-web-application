a6fb13806a6dd4bcc9b0b30ae1108ef6fb227dc4
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-06 00:20:36 +0100

    Spring security configurations detailed corrections in check by ENGR. IPAYE

---
+++ src/main/java/org/engripaye/securepublicrestapimobilewebapplication/config/AudienceValidator.java
@@ -0,0 +1,26 @@
+package org.engripaye.securepublicrestapimobilewebapplication.config;
+
+import org.springframework.security.oauth2.core.OAuth2Error;
+import org.springframework.security.oauth2.core.OAuth2Token;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidatorResult;
+import org.springframework.security.oauth2.jwt.Jwt;
+
+public class AudienceValidator implements OAuth2TokenValidator<Jwt> {
+
+    private final String audience;
+
+    public AudienceValidator(String audience) {
+        this.audience = audience;
+    }
+
+
+    @Override
+    public OAuth2TokenValidatorResult validate(Jwt jwtToken) {
+        if(jwtToken.getAudience().contains(audience)){
+            return OAuth2TokenValidatorResult.success();
+        }
+
+        return OAuth2TokenValidatorResult.failure(new OAuth2Error("invalid_token", "The required audience is missing", null));
+    }
+}
